db745058346b107d79c15c1047b779831f45ffbd
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-04-22 16:52:57 +0600

    Intro section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Intro from "./components/Intro/Intro.jsx";
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
