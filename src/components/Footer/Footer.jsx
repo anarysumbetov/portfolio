9092896dbd832ee796bd15ed1ddc1745127e6ee7
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-05-27 19:46:19 +0600

    some links added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
 import GitHub from "@iconscout/react-unicons/icons/uil-github";
+import LinkedIn from "@iconscout/react-unicons/icons/uil-linkedin";
 
 const Footer = () => {
   return (
@@ -9,8 +10,13 @@ const Footer = () => {
       <img src={Wave} alt="wave-png" style={{ width: "100%" }} />
       <div className="f-content">
         <span>brodiaga_sirim@mail.ru</span>
-        <div className="f-icons">
-          <GitHub color="white" size={"3rem"} />
+        <div className="f-icons"> 
+          <a href="https://github.com/anarysumbetov" rel="noopener noreferrer" target="_blank">
+            <GitHub color="white" size={"3rem"} />
+          </a>
+          <a href="https://www.linkedin.com/in/anarys-umbetov/" rel="noopener noreferrer" target="_blank">
+            <LinkedIn color="white" size={"3rem"} />
+          </a>
         </div>
       </div>
     </div>
